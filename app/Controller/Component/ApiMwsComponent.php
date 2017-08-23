d0572013f12c283ecc672f77c1920b3cb6066ee0
Fictionsoft CEO <mizan92cse@gmail.com>
2017-08-23 23:05:39 +0600

    Mizan: Email Template Added

---
+++ app/Controller/Component/ApiMwsComponent.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mizan
+ * Date: 22/10/14
+ * Time: 16:53
+ */
+
+App::import('Vendor', 'mws', array('file' => 'amazon-mws-master/vendor/autoload.php' ));
+
+
+App::uses('Component', 'Controller');
+class ApiMwsComponent extends Component {
+
+    public $components = array('Session');
+
+    var $controller;
+
+    public function __construct() {
+
+    }
+
+    function startup(Controller $controller){
+        $this->controller = $controller;
+    }
+
+    function getListOrders(){
+        $client = new MCS\MWSClient([
+            'Marketplace_Id' => 'ATVPDKIKX0DER',
+            'Seller_Id' => 'A10YV6NTBY6VOS',
+            'Access_Key_ID' => 'AKIAJIEKAODR7KSYYMXQ',
+            'Secret_Access_Key' => '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',
+            'MWSAuthToken' => '' // Optional. Only use this key if you are a third party user/developer
+        ]);
+
+        // Optionally check if the supplied credentials are valid
+        if ($client->validateCredentials()) {
+            // Credentials are valid
+        } else {
+            // Credentials are not valid
+        }
+
+
+        //$t1 = date("c", time()-1*24*60*60);
+        $fromDate = new DateTime('2017-05-05');
+        $orders = $client->ListOrders($fromDate, true);
+
+        foreach ($orders as $order) {
+            $items = $client->ListOrderItems($order['AmazonOrderId']);
+            print_r($order);
+            print_r($items);
+        }
+    }
+
+
+}
+
